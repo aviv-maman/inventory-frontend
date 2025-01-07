8f2a43c407c910fae5ff385a4a5f9926b2703a43
Aviv Maman <awiwmaman@outlook.com>
2025-01-07 09:47:04 +0200

    deps

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import SpinnerWave from '@/components/SpinnerWave';
 
 export default function Loading() {
   return (
-    <section className='container relative flex min-h-[calc(100vh-150px)] flex-col items-center justify-around p-6 sm:min-h-[calc(100vh-142px)] sm:justify-evenly sm:px-8'>
+    <section className='relative flex min-h-[calc(100vh-150px)] flex-col items-center justify-around p-6 sm:min-h-[calc(100vh-142px)] sm:justify-evenly sm:px-8'>
       <SpinnerWave />
       <div />
     </section>
