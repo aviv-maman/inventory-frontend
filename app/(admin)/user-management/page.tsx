0cccbe54e66ec5bb41ac926ea77b2c8593f12781
Aviv Maman <awiwmaman@outlook.com>
2025-01-18 11:30:26 +0200

    Update page.tsx

--- app/(admin)/user-management/page.tsx
+++ app/(admin)/user-management/page.tsx
@@ -9,7 +9,7 @@ export default async function UserManagementPage() {
 
   return (
     <section className='flex size-full flex-col items-center justify-center'>
-      <div className='fixed top-6 flex min-w-96 max-w-96 flex-col items-center gap-7 rounded-md border p-6 sm:max-w-7xl'>
+      <div className='fixed top-6 flex min-w-96 max-w-96 flex-col items-center gap-7 rounded-md border p-6 sm:max-w-[608px] md:max-w-2xl lg:max-w-7xl'>
         <h1 className='w-fit rounded border px-4 py-2 text-center text-2xl font-bold tracking-tight'>
           User Management
         </h1>
